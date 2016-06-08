7c668ed6816c2dc0e1675d654812b95b7b60a09d
QuanticPotatoes <jeremy.dieuze@viacesi.fr>
2016-06-08 14:53:29 +0200

    Remake of the main file

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char ** argv) {
   double * in;
   fftw_complex *out;
   int half = N/2;
-  double processed[half];
+  double *processed;
   PIXEL * img;
   int x = 0;
   unsigned int color;
@@ -37,21 +37,22 @@ int main(int argc, char ** argv) {
   std::cout << "sample rate: " << sfinfo.samplerate << "\n";
 
 
-  img = new PIXEL[N * (sfinfo.frames/N)];
+  img = new PIXEL[half * (sfinfo.frames/half)];
  
   in = (double*) fftw_malloc(sizeof(double) * N);
   out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-  
+  processed = (double*) fftw_malloc(sizeof(double) * N);
+
   p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE);
   
   while((readcount = sf_read_double(infile,in,N))){
-
+  
   	fftw_execute(p); /* repeat as needed */
   	
-  	for(int i = 0; i < half; i++) {
+  	for(int i = 0; i < N; i++) {
 
-     out[i][0] *= N; // Real Values
-     out[i][1] *= N; // Complex Values
+     out[i][0] *= half; // Real Values
+     out[i][1] *= half; // Complex Values
 
      processed[i] = out[i][0]*out[i][0] + out[i][1]*out[i][1]; // Power Spectrum
      processed[i] = 10./log(10.) * log(processed[i] + 1e-6); // dB
@@ -61,25 +62,21 @@ int main(int argc, char ** argv) {
 		   		processed[i] = 0;
 		   	}
 
-   }
-
-   }
- 
- 	for(int x = 0; x < half; x++) {
+		  color =  (log (processed[i] + 1e-6)/log(10)) /-60. *255*255*128;
+      //color = (int) ( 10 * ( log10( 2 * ( pow((processed[i] * 255 * 255 ) * ( 2 /  (double) N ) , 2) ))) / -60   );
+      //color = (int) ( 10 * ( log10( 2 * ( pow((processed[i] * 255 ) ,2)))));
+          //std::cout << x + sfinfo.frames - i * (sfinfo.frames/half) << "\n";
+          img[x + i * (sfinfo.frames/N)].Red = (color & 255);
+		      img[x + i * (sfinfo.frames/N)].Green = ((color & (255 << 8)) >> 8);
+		      img[x + i * (sfinfo.frames/N)].Blue = ((color & (255 << 16)) >> 16);
+		      img[x + i * (sfinfo.frames/N)].Alpha = 255;
     
-	    for(int i = 0; i < half ; i++) {
-
-		     color = processed[i]* 255 *255 * 255;
-
-		      img[x + sfinfo.frames - i * (sfinfo.frames/half)].Green = ((color & (255 << 8)) >> 8);
-		      img[x + sfinfo.frames - i * (sfinfo.frames/half)].Blue = ((color & (255 << 16)) >> 16);
-		      img[x + sfinfo.frames - i * (sfinfo.frames/half)].Alpha = 255;
-	    }
-    x++;
+      }
+      x++;
   };
 
 
-  writePng(img, (sfinfo.frames/N), N);
+  writePng(img, (sfinfo.frames/N), half);
  
   // ...
 
